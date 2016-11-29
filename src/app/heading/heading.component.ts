9871754c75a5225e72d5f5f70106d1067ff6c6c2
Yusuf Fadairo <yusuf.fadairo@scotiabank.com>
2016-11-29 10:49:32 -0500

    adding Heading component

---
+++ src/app/heading/heading.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'yus-app-heading',
+  templateUrl: './heading.component.html',
+  styleUrls: ['./heading.component.css']
+})
+export class HeadingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
