65e2301964d47b2cb31f2fd3f428ff4906b32316
Yusuf Fadairo <yusuf.fadairo@scotiabank.com>
2016-11-29 10:44:19 -0500

    updating root app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  post = {
+    title: "Tech Giant Invests Huge Money to Build a Computer Out of Science Fiction",
+    body: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus quis error, a aliquid porro quia sint eaque laboriosam itaque perspiciatis officia sunt dicta, eius esse deleniti? Sapiente dolore, asperiores sint."
+  }
 }
