3f779038db8bfee216ddb44ebaf5900c780e4cca
yusuf-kami <yusuf.kami@gmail.com>
2016-11-30 10:16:41 -0500

    adding searchbar component

---
+++ src/app/searchbar/searchbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'yus-app-searchbar',
+  templateUrl: './searchbar.component.html',
+  styleUrls: ['./searchbar.component.css']
+})
+export class SearchbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
